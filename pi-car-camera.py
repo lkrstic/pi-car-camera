70ee57b683ed2ffe6f61e92180d3efdc74678ae2
Lana <lanakrstic@gmail.com>
2020-10-19 20:55:37 -0400

    removed secret key, reworked code for clarity

--- pi-car-camera.py
+++ pi-car-camera.py
@@ -5,18 +5,13 @@ from datetime import datetime
 import requests
 import base64
 import json
-
-# initialize hardware
-button = Button(25)
-redLED = LED(17)
-greenLED = LED(22)
-camera = PiCamera()
-redLED.on()
+import secretkey
 
 # Takes an image, saves with current timestamp,
 # and passes it to AnalyzeImage()
 def CaptureImage():
     imgName = datetime.now().isoformat() + '.jpg'
+    
     print(imgName)
     camera.capture('%s' % imgName)
     AnalyzeImage(imgName)
@@ -30,32 +25,40 @@ def CaptureImage():
 def AnalyzeImage(imgToCheck):
     print(imgToCheck)
     #TO REMOVE, test image
-    imgToCheck = 'nocartest.jpg'
+    imgToCheck = 'cartest.jpg'
     print(imgToCheck)
-    SECRET_KEY = ' sk_fce59eaab37d90ea2d5dc63a'
-    ALPRurl = 'https://api.openalpr.com/v3/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' % (SECRET_KEY)
+    ALPRurl = 'https://api.openalpr.com/v3/recognize_bytes?recognize_vehicle=1&country=us&return_image=1&secret_key=%s' % (secretkey.SECRET_KEY)
     # ALPRurl = 'https://api.openalpr.com/v3/recognize_bytes?country=us&secret_key=%s' % (SECRET_KEY)
     with open(imgToCheck, 'rb') as imgFile:
         imgBase64 = base64.b64encode(imgFile.read())
         
     response = requests.post(ALPRurl, data = imgBase64)
     jsonResponse = response.json()
-    print(jsonResponse["results"])
+    with open('ResponseALPR.json', 'w') as write_file:
+         json.dump(response.json(), write_file, indent=2)
+         
     if len(jsonResponse["results"]) != 0:
         print("not empty")
-        #todo prepare object for sending to server
-
-#OLD        
-#     with open('ResponseALPR.json', 'w') as write_file:
-#         json.dump(response.json(), write_file, indent=2)
+        #TODO: prepare object for sending to server
 
 
 # Sends the results to our web server for authorization.
 # If there is a match in the db, the vehicle is authorized.
 def AuthorizeVehicle():
-    #todo add POST to web application
+    #TODO add POST to web application
     pass
 
+def Cleanup():
+    pass
+    #TODO: cleanup
+
+
+# initialize hardware
+button = Button(25)
+redLED = LED(17)
+greenLED = LED(22)
+camera = PiCamera()
+redLED.on()
 
 
 button.when_pressed = CaptureImage
