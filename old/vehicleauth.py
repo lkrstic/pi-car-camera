41b7c23cc85184fdcdc56f8c699cc7bbfd79710e
lkrstic <9921871+lkrstic@users.noreply.github.com>
2020-12-07 22:15:58 -0500

    Delete vehicleauth.py

--- old/vehicleauth.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-import secretkey
-
-ALPR_url = 'https://api.openalpr.com/v3/recognize_bytes?recognize_vehicle=1&country=us&return_image=1&secret_key=%s' % (secretkey.SECRET_KEY)
-application_url = 'https://my-python-project.azurewebsites.net/image/save'
-
